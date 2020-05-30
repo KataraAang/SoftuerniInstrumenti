1df755c55d85909cdfce12deeea52b27268adeec
Tagrid <tagridottii@gmail.com>
2020-05-30 19:47:13 +0300

    org.springframework.boot.web.servlet.support.SpringBootServletInitializer

--- src/main/java/com/example/demo/SoftuerniInstrumentiApplication.java
+++ src/main/java/com/example/demo/SoftuerniInstrumentiApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class SoftuerniInstrumentiApplication {
+public class SoftuerniInstrumentiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoftuerniInstrumentiApplication.class, args);
