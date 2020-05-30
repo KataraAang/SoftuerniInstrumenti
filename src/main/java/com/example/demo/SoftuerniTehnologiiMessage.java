29e3c80c581abb23e7a7f3c5015720d65e0fa92a
Tagrid <tagridottii@gmail.com>
2020-05-30 18:41:52 +0300

    Project A commit

---
+++ src/main/java/com/example/demo/SoftuerniTehnologiiMessage.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SoftuerniTehnologiiMessage {
+
+	@RequestMapping("/stMessage")
+	public @ResponseBody String softuerniTehnologiiMessage() {
+			
+		return "Privet, gospodin Malinov :)";
+		
+	}
+
+	
+}
